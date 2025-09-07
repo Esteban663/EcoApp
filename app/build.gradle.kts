ef163c23f1776636bf018fd9253f036161886303
Esteban <estebanarroyave663@gmail.com>
2025-09-07 18:17:42 -0500

    Primer Commit Menu y Mapa

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+}
+
+android {
+    namespace = "com.example.ecoapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.ecoapp"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Dependencias osmdroid
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
+    implementation("org.osmdroid:osmdroid-wms:6.1.14")
+}
---
+++ app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ecoapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+    {
+        private Button btnMapa, btnConsejos;
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+            {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                
+                btnMapa = findViewById(R.id.btnMapa);
+                btnConsejos = findViewById(R.id.btnConsejos);
+     
+            }
+    }
